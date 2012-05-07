eb0ff8fc5e8ec82efb0ba84bb89f3fb1d18c32d4
Stephen Orban <sorban@gmail.com>
2012-05-07 15:40:53 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpotechRails::Application.config.secret_token = '3c473b297669f782d8b41b262b523d398186eb76d52aabf40745599110fa437e82dfc939ff98ef4c0e8c1259b51265263e467058093af65bb73ca3ad33a75edc'
